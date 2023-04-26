3a7da8bf03c9e40948f343413101bbe715fea9a6
VanikSarajyan <vaniksarajyan@gmail.com>
2023-04-26 10:52:27 +0400

    cors and requirements

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .models import Base
 from .database import engine
@@ -8,6 +9,16 @@ app = FastAPI()
 
 # #No longer needed because of Alembic
 # Base.metadata.create_all(bind=engine)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.posts_router)
 app.include_router(user.users_router)
